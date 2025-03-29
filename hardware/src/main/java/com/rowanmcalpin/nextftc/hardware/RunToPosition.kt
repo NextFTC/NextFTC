3bede803d1b5e1a12ec45ad82a98e14aaea9f592
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-03-29 18:02:57 -0500

    chore: Update copyright
    - Add copyright notice to files without it
    - Add copyright notice to IntelliJ settings in VCS so that other people can use it
    - Format files

--- hardware/src/main/java/com/rowanmcalpin/nextftc/hardware/RunToPosition.kt
+++ hardware/src/main/java/com/rowanmcalpin/nextftc/hardware/RunToPosition.kt
@@ -1,19 +1,19 @@
 /*
-NextFTC: a user-friendly control library for FIRST Tech Challenge
-    Copyright (C) 2025 Rowan McAlpin
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * NextFTC: a user-friendly control library for FIRST Tech Challenge
+ *     Copyright (C) 2025 Rowan McAlpin
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.rowanmcalpin.nextftc.hardware
@@ -36,12 +36,45 @@ class RunToPosition @JvmOverloads constructor(
     system: ControlSystem,
     goal: Double,
     tolerance: KineticState = KineticState(10.0, 5.0, Double.POSITIVE_INFINITY),
-    subsystems: Set<Subsystem> = emptySet()): com.rowanmcalpin.nextftc.hardware.RunToState(system, KineticState(goal), tolerance, subsystems) {
+    subsystems: Set<Subsystem> = emptySet()
+) : RunToState(
+    system,
+    KineticState(goal),
+    tolerance,
+    subsystems
+) {
 
     @JvmOverloads
-    constructor(system: ControlSystem, goal: Double, positionTolerance: Double, subsystems: Set<Subsystem> = emptySet()): this(system, goal, KineticState(positionTolerance, 5.0, Double.POSITIVE_INFINITY), subsystems)
+    constructor(
+        system: ControlSystem,
+        goal: Double,
+        positionTolerance: Double = 10.0,
+        subsystems: Set<Subsystem> = emptySet()
+    ) : this(
+        system,
+        goal,
+        KineticState(positionTolerance, 5.0, Double.POSITIVE_INFINITY),
+        subsystems
+    )
 
-    constructor(system: ControlSystem, goal: Double, tolerance: KineticState = KineticState(10.0, 5.0, Double.POSITIVE_INFINITY), subsystem: Subsystem): this(system, goal, tolerance, setOf(subsystem))
+    @JvmOverloads
+    constructor(
+        system: ControlSystem,
+        goal: Double,
+        tolerance: KineticState = KineticState(10.0, 5.0, 5.0),
+        subsystem: Subsystem
+    ) : this(system, goal, tolerance, setOf(subsystem))
 
-    constructor(system: ControlSystem, goal: Double, positionTolerance: Double, subsystem: Subsystem): this(system, goal, KineticState(positionTolerance, 5.0, Double.POSITIVE_INFINITY), setOf(subsystem))
+    @JvmOverloads
+    constructor(
+        system: ControlSystem,
+        goal: Double,
+        positionTolerance: Double = 10.0,
+        subsystem: Subsystem
+    ) : this(
+        system,
+        goal,
+        KineticState(positionTolerance, 5.0, Double.POSITIVE_INFINITY),
+        setOf(subsystem)
+    )
 }
