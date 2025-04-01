aac2248075cb2611e31e313a86d5508bb08bec8a
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-03-31 20:31:32 -0500

    feat: Create IfElseCommand and SwitchCommand
    - Rename BlockingConditionalCommand to IfElseCommand
    - Rename BlockingSwitchCommand to SwitchCommand
    - Modify the constructor of IfElseCommand and SwitchCommand from taking command suppliers to taking commands
    - Make IfElseCommand and SwitchCommand run the selected command themself instead of delegating it to CommandManager
    - Create SwitchCommand#withCase and SwitchCommand#withDefault
    
    BREAKING CHANGE: BlockingConditionalCommand and BlockingSwitchCommand have been renamed and have new constructors.

--- core/src/main/java/com/rowanmcalpin/nextftc/core/command/utility/conditionals/SwitchCommand.kt
+++ core/src/main/java/com/rowanmcalpin/nextftc/core/command/utility/conditionals/SwitchCommand.kt
@@ -18,49 +18,40 @@
 
 package com.rowanmcalpin.nextftc.core.command.utility.conditionals
 
+import android.widget.Switch
 import com.rowanmcalpin.nextftc.core.command.Command
 import com.rowanmcalpin.nextftc.core.command.CommandManager
+import com.rowanmcalpin.nextftc.core.command.utility.NullCommand
 
 /**
  * This behaves like the command-form of a switch statement. You provide it with a value to
- * reference, and a list of options of outcomes. It is blocking, meaning `isDone` will not return 
+ * reference, and a list of options of outcomes. It is blocking, meaning `isDone` will not return
  * `true` until the scheduled command(s) have completed running.
  * @param value the value to reference
  * @param outcomes all of the options for outcomes
  * @param default the command to schedule if none of the outcomes are fulfilled
  */
-class BlockingSwitchCommand(
-    private val value: () -> Any,
-    private vararg val outcomes: Pair<Any, () -> Command>,
-    private val default: (() -> Command)? = null
-): Command() {
+class SwitchCommand<T> @JvmOverloads constructor(
+    private val value: () -> T,
+    private val outcomes: Map<T, Command> = emptyMap(),
+    private val default: Command = NullCommand()
+) : Command() {
 
-    /**
-     * The commands that have been selected
-     */
-    private val selectedCommands: MutableList<Command> = mutableListOf()
+    private lateinit var selectedCommand: Command
 
-    /**
-     * Returns true once every executing command is done
-     */
-    override val isDone: Boolean
-        get() = selectedCommands.all { it.isDone }
-    
+    override val isDone: Boolean by selectedCommand::isDone
+
     override fun start() {
-        val value = this.value.invoke()
-        outcomes.forEach {
-            if (it.first == value) {
-                selectedCommands.add(it.second.invoke())
-                CommandManager.scheduleCommand(selectedCommands.last())
-            }
-        }
-        
-        if (selectedCommands.size == 0) {
-            if (default != null) {
-                selectedCommands.add(default.invoke())
-                CommandManager.scheduleCommand(selectedCommands.last())
-            }
-        }
+        selectedCommand = outcomes[value()] ?: default
+        selectedCommand.start()
     }
-    
+
+    override fun update() = selectedCommand.update()
+
+    override fun stop(interrupted: Boolean) = selectedCommand.stop(interrupted)
+
+    fun withCase(case: T, command: Command) =
+        SwitchCommand(value, outcomes + (case to command), default)
+
+    fun withDefault(command: Command) = SwitchCommand(value, outcomes, command)
 }
