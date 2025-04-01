6c423126afc218770401584396f3e2b4c1fc75e2
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-03-31 20:31:32 -0500

    feat: Create IfElseCommand and SwitchCommand
    - Rename BlockingConditionalCommand to IfElseCommand
    - Rename BlockingSwitchCommand to SwitchCommand
    - Modify the constructor of IfElseCommand and SwitchCommand from taking command suppliers to taking commands
    - Make IfElseCommand and SwitchCommand run the selected command themself instead of delegating it to CommandManager
    - Create SwitchCommand#withCase and SwitchCommand#withDefault
    
    BREAKING CHANGE: BlockingConditionalCommand and BlockingSwitchCommand have been renamed and have new constructors.

--- core/src/main/java/com/rowanmcalpin/nextftc/core/command/utility/conditionals/IfElseCommand.kt
+++ core/src/main/java/com/rowanmcalpin/nextftc/core/command/utility/conditionals/IfElseCommand.kt
@@ -20,40 +20,32 @@ package com.rowanmcalpin.nextftc.core.command.utility.conditionals
 
 import com.rowanmcalpin.nextftc.core.command.Command
 import com.rowanmcalpin.nextftc.core.command.CommandManager
+import com.rowanmcalpin.nextftc.core.command.utility.NullCommand
 
 /**
- * This command behaves as an `if` statement, and schedules commands based on the result of the if 
- * statement. It is blocking, meaning `isDone` will not return `true` until the scheduled command 
+ * This command behaves as an `if` statement, and schedules commands based on the result of the if
+ * statement. It is blocking, meaning `isDone` will not return `true` until the scheduled command
  * has completed running.
  * @param condition the condition to reference
  * @param trueCommand the command to schedule if the reference is true
  * @param falseCommand the command to schedule if the reference is false
  */
-class BlockingConditionalCommand(
+class IfElseCommand @JvmOverloads constructor(
     private val condition: () -> Boolean,
-    private val trueCommand: () -> Command,
-    private val falseCommand: (() -> Command)? = null
-): Command() {
+    private val trueCommand: Command,
+    private val falseCommand: Command = NullCommand()
+) : Command() {
 
     private lateinit var selectedCommand: Command
 
-    override val isDone: Boolean
-        get() {
-            if (this::selectedCommand.isInitialized) {
-                return selectedCommand.isDone
-            }
-            return false
-        }
+    override val isDone: Boolean by selectedCommand::isDone
 
     override fun start() {
-        if (condition.invoke()) {
-            selectedCommand = trueCommand.invoke()
-            CommandManager.scheduleCommand(selectedCommand)
-        } else {
-            if (falseCommand != null) {
-                selectedCommand = falseCommand.invoke()
-                CommandManager.scheduleCommand(selectedCommand)
-            }
-        }
+        selectedCommand = if (condition()) trueCommand else falseCommand
+        selectedCommand.start()
     }
+
+    override fun update() = selectedCommand.update()
+
+    override fun stop(interrupted: Boolean) = selectedCommand.stop(interrupted)
 }
