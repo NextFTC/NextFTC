23c7551071c78c23c40c0d3019f0b8fbbfd18dde
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-04-02 13:10:19 -0500

    refactor(core): Clean up SubsystemComponent
    - Make SubsystemComponent#subsystems a val instead of a var
    - Move expanding subsystems from preInit to a property initializer
    - Extract updateSubsystems to a private function (it is used in preWaitForStart and preUpdate
    - Inline initSubsystems (it is only used in preInit and it is now all that preInit does)
    - Use subsystems.flatMap { it.subsystems } to expand subsystems instead of doing it imperatively in SubsystemComponent#expandSubsystems
    
    BREAKING CHANGE: SubsystemComponent#subsystems is now immutable

--- core/src/main/java/com/rowanmcalpin/nextftc/core/components/SubsystemComponent.kt
+++ core/src/main/java/com/rowanmcalpin/nextftc/core/components/SubsystemComponent.kt
@@ -19,84 +19,28 @@
 package com.rowanmcalpin.nextftc.core.components
 
 import com.rowanmcalpin.nextftc.core.subsystems.Subsystem
-import com.rowanmcalpin.nextftc.core.subsystems.SubsystemGroup
 import com.rowanmcalpin.nextftc.core.command.CommandManager
 
 /**
  * This component adds [Subsystem]s to your OpMode
  */
-class SubsystemComponent(vararg var subsystems: Subsystem): NextComponent {
+class SubsystemComponent(vararg subsystems: Subsystem) : Component {
+    val subsystems = subsystems.flatMap { it.subsystems }.toSet()
+
     override fun preInit() {
-        subsystems = expandSubsystems(subsystems.toSet()).toTypedArray()
-        initSubsystems()
-    }
-
-    override fun preWaitForStart() {
-        subsystems.forEach {
-            it.periodic()
-
-            // Check if there are any commands running that use the subsystem, or if we can safely
-            // schedule its default command
-            if (!CommandManager.hasCommandsUsing(it)) {
-                CommandManager.scheduleCommand(it.defaultCommand)
-            }
-        }
-    }
-
-    override fun preUpdate() {
-        subsystems.forEach {
-            it.periodic()
-
-            // Check if there are any commands running that use the subsystem, or if we can safely
-            // schedule its default command
-            if (!CommandManager.hasCommandsUsing(it)) {
-                CommandManager.scheduleCommand(it.defaultCommand)
-            }
-        }
-    }
-
-    /**
-     * Called internally to initialize subsystems.
-     */
-    private fun initSubsystems() {
         subsystems.forEach {
             it.initialize()
         }
     }
 
-    /**
-     * Given a set of subsystems (including groups), this extracts the subsystems from within those
-     * groups and returns the set of the subsystems in the set plus the subsystems that are children
-     * of the groups (if any).
-     * @param subsystems the set of subsystems to expand
-     * @return the expanded set of subsystems
-     */
-    private fun expandSubsystems(subsystems: Set<Subsystem>): Set<Subsystem> {
-        val expanded = mutableListOf<Subsystem>()
+    override fun preWaitForStart() = updateSubsystems()
 
-        for (subsystem in subsystems) {
-            if (subsystem is SubsystemGroup) {
-                expanded += expandSubsystemGroup(subsystem)
-            }
-            expanded += subsystem
+    override fun preUpdate() = updateSubsystems()
+
+    private fun updateSubsystems() {
+        subsystems.forEach {
+            it.periodic()
+            if (!CommandManager.hasCommandsUsing(it)) it.defaultCommand.schedule()
         }
-
-        return expanded.toSet()
-    }
-
-    /**
-     * Expands a subsystem group (recursively)
-     */
-    private fun expandSubsystemGroup(group: SubsystemGroup): List<Subsystem> {
-        val expanded = mutableListOf<Subsystem>()
-
-        for (child in group.subsystems) {
-            if (child is SubsystemGroup) {
-                expanded += expandSubsystemGroup(child)
-            }
-            expanded += child
-        }
-
-        return expanded
     }
 }
