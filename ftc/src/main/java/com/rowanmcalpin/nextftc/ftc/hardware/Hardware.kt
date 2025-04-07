e99c5f0791b436ec9e135b6e3f774fd372b2a058
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-04-06 19:42:34 -0500

    feat(ftc): Add Hardware and Caching
    - Hardware is an abstract class that hardware will extend. It adds lazily creating the SDK hardware
    - Caching is a delegate for a Double that implements caching

---
+++ ftc/src/main/java/com/rowanmcalpin/nextftc/ftc/hardware/Hardware.kt
@@ -0,0 +1,54 @@
+/*
+ * NextFTC: a user-friendly control library for FIRST Tech Challenge
+ *     Copyright (C) 2025 Rowan McAlpin
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.rowanmcalpin.nextftc.ftc.hardware
+
+import kotlin.math.abs
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+abstract class Hardware<T>(initializer: () -> T) {
+    val hardware: T by lazy { initializer() }
+}
+
+class Caching(
+    initialValue: Double,
+    private val cacheTolerance: Double,
+    private val whenSet: (Double?) -> Unit
+) : ReadWriteProperty<Any?, Double> {
+
+    private var cachedValue = initialValue
+
+    override fun getValue(
+        thisRef: Any?,
+        property: KProperty<*>
+    ): Double = cachedValue
+
+    override fun setValue(
+        thisRef: Any?,
+        property: KProperty<*>,
+        value: Double
+    ) {
+        if (abs(cachedValue - value) > cacheTolerance) {
+            cachedValue = value
+            whenSet(value)
+        } else {
+            whenSet(null)
+        }
+    }
+}
