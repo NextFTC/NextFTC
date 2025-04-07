abf7598368b3a048696a69eb7a5539830d8c23b3
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-04-06 19:42:34 -0500

    feat(ftc): Add Hardware and Caching
    - Hardware is an abstract class that hardware will extend. It adds lazily creating the SDK hardware
    - Caching is a delegate for a Double that implements caching

--- ftc/src/main/java/com/rowanmcalpin/nextftc/ftc/hardware/MotorEx.kt
+++ ftc/src/main/java/com/rowanmcalpin/nextftc/ftc/hardware/MotorEx.kt
@@ -16,47 +16,56 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.rowanmcalpin.nextftc.ftc.hardware.controllables
+package com.rowanmcalpin.nextftc.ftc.hardware
 
 import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior
 import com.qualcomm.robotcore.hardware.DcMotorEx
-import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction
 import com.rowanmcalpin.nextftc.ftc.OpModeData
 import com.rowanmcalpin.nextftc.hardware.controllable.Controllable
-import kotlin.math.abs
 
 /**
  * Wrapper class for motors that implements controllable (and can therefore be used with RunToPosition
  * commands).
  */
-class MotorEx(val motor: DcMotorEx) : Controllable {
+class MotorEx(cacheTolerance: Double, motorFactory: () -> DcMotorEx) :
+    Hardware<DcMotorEx>(motorFactory), Controllable {
 
-    constructor(name: String) : this(OpModeData.hardwareMap!!.get(DcMotorEx::class.java, name))
+    constructor(motorFactory: () -> DcMotorEx) : this(0.01, motorFactory)
 
-    private var cachedPower = 0.0 // By default, motors are unpowered
+    @JvmOverloads
+    constructor(motor: DcMotorEx, cacheTolerance: Double = 0.01) : this(cacheTolerance, { motor })
 
-    private var tickOffset = 0.0 // By default, we don't want to offset from the motor's raw ticks
+    @JvmOverloads
+    constructor(
+        name: String,
+        cacheTolerance: Double = 0.01
+    ) : this(cacheTolerance, { OpModeData.hardwareMap!![DcMotorEx::class.java, name] })
 
-    /**
-     * The tolerance that must be surpassed in order to update the motors power. Defaults to 0.01.
-     */
-    var cacheTolerance = 0.01
+    private var offset = 0.0
+    private var center = 0.0
+    var direction = 1
+        set(value) {
+            require(
+                value in intArrayOf(
+                    -1,
+                    1
+                )
+            ) { "direction must be either 1 or -1 but was $value." }
+            center = currentPosition
+            field = value
+        }
 
     /**
      * Gives the unmodified raw tick value of the motor
      */
     val rawTicks: Double
-        get() = motor.currentPosition.toDouble()
+        get() = hardware.currentPosition.toDouble()
 
     /**
      * Zero Power Behavior for the motor
      */
-    var zeroPowerBehavior: ZeroPowerBehavior
-        get() = motor.zeroPowerBehavior
-        set(value) {
-            motor.zeroPowerBehavior = value
-        }
+    var zeroPowerBehavior: DcMotor.ZeroPowerBehavior by hardware::zeroPowerBehavior
+
 
     /**
      * This returns the current position of the motor, accounting for any offsets created by manually
@@ -66,43 +75,37 @@ class MotorEx(val motor: DcMotorEx) : Controllable {
      * which will get accounted for automatically when getting this value.
      */
     override var currentPosition: Double
-        get() = (motor.currentPosition.toDouble() - tickOffset)
+        get() = (offset + rawTicks - center) * direction + center
         set(value) {
-            tickOffset = rawTicks - value
+            center = value
+            offset = rawTicks - value
         }
 
     /**
-     * Current velocity of the motor. Setter does nothing
+     * Current velocity of the motor
      */
-    override var velocity: Double
-        get() = motor.velocity
-        set(value) {}
-
-    var direction: Direction
-        get() = motor.direction
-        set(value) {
-            motor.direction = value
-        }
+    override val velocity: Double by hardware::velocity
 
     /**
      * Gets / sets the current power of the motor (automatically implements power caching)
      */
-    override var power: Double
-        get() = cachedPower
-        set(value) {
-            motor.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-            if (abs(cachedPower - value) > cacheTolerance) {
-                motor.power = value
-                cachedPower = value
-            }
-        }
-
-    fun reverse(): MotorEx {
-        direction = Direction.REVERSE
-        return this
+    override var power: Double by Caching(0.0, cacheTolerance) {
+        hardware.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        it?.let { hardware.power = it }
     }
 
-    fun resetEncoder() {
+    fun reverse() {
+        direction = -direction
+    }
+
+    fun reversed() = apply { reverse() }
+
+    fun zero() {
         currentPosition = 0.0
     }
+
+    fun zeroed() = apply { zero() }
+
+    fun floatMode() = apply { zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT }
+    fun brakeMode() = apply { zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE }
 }
