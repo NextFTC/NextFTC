14cd0daa3be433938da5f8f53b86e11f33b91d3d
Zach Harel <zach@zharel.me>
2025-07-26 01:20:41 -0400

    add serializers for command logging and snapshots
    for now, these will only be used for writing to logcat,
    but i am providing capability to log to json files for example
    
    Signed-off-by: Zach Harel <zach@zharel.me>

--- core/src/main/kotlin/dev/nextftc/core/command/Logging.kt
+++ core/src/main/kotlin/dev/nextftc/core/command/Logging.kt
@@ -1,21 +1,90 @@
 package dev.nextftc.core.command
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.encoding.encodeStructure
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.DurationUnit
 
-object CommandSerializer : KSerializer<Command> {
+/**
+ * Generates a serialized format of a Command using its class name
+ * and its requirements' class names.
+ *
+ * Cannot be used for deserialization.
+ */
+internal object CommandLogSerializer : KSerializer<Command> {
     override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Command") {
-        element()
+        element<String>("name")
+        element<String>("requirements")
     }
 
-    override fun serialize(encoder: Encoder, value: Command) {
-        TODO("Not yet implemented")
-    }
+    override fun serialize(encoder: Encoder, value: Command) =
+        encoder.encodeStructure(descriptor) {
+            encodeStringElement(descriptor, 0, value::class.simpleName?: "Unknown Command")
+            encodeStringElement(descriptor, 1, value.requirements.map { it::class.simpleName?: "Unknown Requirement" }.toString())
+        }
 
     override fun deserialize(decoder: Decoder): Command {
-        TODO("Not yet implemented")
+        error { "THIS SHOULD ONLY BE USED AS A SERIALIZER FOR LOGGING; DO NOT USE FOR DESERIALIZATION" }
     }
 }
+
+/**
+ * Serializes a command snapshot
+ * (a list of commands running at an instance),
+ * using [CommandLogSerializer].
+ *
+ * Cannot be used for deserialization.
+ */
+internal object CommandSnapshotSerializer : KSerializer<List<Command>> {
+    val ls = ListSerializer(CommandLogSerializer)
+
+    override val descriptor: SerialDescriptor = ls.descriptor
+
+    override fun serialize(encoder: Encoder, value: List<Command>) {
+        ls.serialize(encoder, value)
+    }
+
+    override fun deserialize(decoder: Decoder): List<Command> {
+        error { "THIS SHOULD ONLY BE USED AS A SERIALIZER FOR LOGGING; DO NOT USE FOR DESERIALIZATION" }
+    }
+}
+
+/**
+ * Serializes and deserializes a duration represented in JSON as "<number>ms"
+ */
+internal object TimestampLogSerializer : KSerializer<Duration> {
+    val primitive = PrimitiveSerialDescriptor("Timestamp", PrimitiveKind.STRING)
+
+    override val descriptor = SerialDescriptor("Timestamp", primitive)
+
+    override fun serialize(encoder: Encoder, value: Duration) {
+        encoder.encodeString("${value.toDouble(DurationUnit.MILLISECONDS)}ms")
+    }
+
+    override fun deserialize(decoder: Decoder): Duration {
+        return decoder.decodeString().removeSuffix("ms").toDouble().milliseconds
+    }
+
+}
+
+/**
+ * Represents a serializable snapshot of commands.
+ *
+ * @param timestamp duration since OpMode start
+ * @param commands commands scheduled at this timestamp
+ */
+@Serializable
+class CommandSnapshot(
+    @Serializable(with = TimestampLogSerializer::class) val timestamp: Duration,
+    @Serializable(with = CommandSnapshotSerializer::class) val commands: List<Command>
+)
