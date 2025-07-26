651229c514369ace5ed1816c9e60bb195cb32470
Zach Harel <zach@zharel.me>
2025-07-26 15:35:10 -0400

    support commandgroup logging for children
    Signed-off-by: Zach Harel <zach@zharel.me>

--- core/src/main/kotlin/dev/nextftc/core/command/Logging.kt
+++ core/src/main/kotlin/dev/nextftc/core/command/Logging.kt
@@ -1,5 +1,7 @@
 package dev.nextftc.core.command
 
+import dev.nextftc.core.command.groups.CommandGroup
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ListSerializer
@@ -8,6 +10,7 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.buildClassSerialDescriptor
 import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.descriptors.listSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.encoding.encodeStructure
@@ -22,15 +25,21 @@ import kotlin.time.DurationUnit
  * Cannot be used for deserialization.
  */
 internal object CommandLogSerializer : KSerializer<Command> {
+    @OptIn(ExperimentalSerializationApi::class)
     override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Command") {
         element<String>("name")
         element<String>("requirements")
+        element<String>("children", isOptional=true)
     }
 
     override fun serialize(encoder: Encoder, value: Command) =
         encoder.encodeStructure(descriptor) {
             encodeStringElement(descriptor, 0, value.name)
             encodeStringElement(descriptor, 1, value.requirements.map { it::class.simpleName?: "Unknown Requirement" }.toString())
+
+            if (value is CommandGroup) {
+                encodeSerializableElement(descriptor, 2, ListSerializer(CommandLogSerializer), value.commands.toList())
+            }
         }
 
     override fun deserialize(decoder: Decoder): Command {
