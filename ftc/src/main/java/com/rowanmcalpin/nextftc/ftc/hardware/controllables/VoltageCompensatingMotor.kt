5c59e826dec4ff695d8ef7850247b0a56072b519
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-08-19 17:34:37 -0500

    feat: add VoltageCompensatingMotor
    - Wraps a Controllable and delegates the implementation of Controllable to it
    - Adds voltage compensation with a customizable nominalVoltage and voltageCacheTime

---
+++ ftc/src/main/java/com/rowanmcalpin/nextftc/ftc/hardware/controllables/VoltageCompensatingMotor.kt
@@ -0,0 +1,42 @@
+package com.rowanmcalpin.nextftc.ftc.hardware.controllables
+
+import com.rowanmcalpin.nextftc.ftc.OpModeData
+import kotlin.properties.Delegates
+import kotlin.time.ComparableTimeMark
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.TimeSource.Monotonic.markNow
+
+class VoltageCompensatingMotor @JvmOverloads constructor(
+    private val controllable: Controllable,
+    private val nominalVoltage: Double = 12.0,
+    private val voltageCacheTime: Duration = 500.milliseconds
+) : Controllable by controllable {
+
+    constructor(
+        controllable: Controllable,
+        nominalVoltage: Double = 12.0,
+        voltageCacheTimeSeconds: Double
+    ) : this(controllable, nominalVoltage, voltageCacheTimeSeconds.seconds)
+
+    private val voltageSensor by lazy { OpModeData.hardwareMap.voltageSensor.first() }
+
+    private var cachedVoltage by Delegates.notNull<Double>()
+    private lateinit var lastVoltageRead: ComparableTimeMark
+
+    private val voltage: Double
+        get() {
+            val currentTime = markNow()
+            val timeSinceLastVoltageRead = currentTime - lastVoltageRead
+            if (!::lastVoltageRead.isInitialized || timeSinceLastVoltageRead >= voltageCacheTime) {
+                cachedVoltage = voltageSensor.voltage
+                lastVoltageRead = currentTime
+            }
+            return cachedVoltage
+        }
+
+    override var power by Delegates.observable(0.0) { _, _, new ->
+        controllable.power = new * nominalVoltage / voltage
+    }
+}
