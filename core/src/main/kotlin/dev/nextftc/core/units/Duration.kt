68bcbd173f55baec062ba2e8d41d4ba6995ca2e3
Zach Harel <zach@zharel.me>
2025-08-21 14:08:50 -0400

    put a period at the end smh

--- core/src/main/kotlin/dev/nextftc/core/units/Duration.kt
+++ core/src/main/kotlin/dev/nextftc/core/units/Duration.kt
@@ -3,7 +3,7 @@ package dev.nextftc.core.units
 import kotlin.time.Duration
 
 /**
- * Parses a provided string into a Duration. Differs from `Duration#parse` because it ignores spaces
+ * Parses a provided string into a Duration. Differs from `Duration#parse` because it ignores spaces.
  */
 fun parseDuration(string: String): Duration {
     return Duration.parse(string.replace("\\s".toRegex(), ""))
