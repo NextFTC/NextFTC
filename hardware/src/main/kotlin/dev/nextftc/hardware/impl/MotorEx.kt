739e22c309b6f66980b73f9d267ed56d68dfefb4
Nick Manor <nmanor@rbaconsulting.com>
2025-09-19 16:48:28 -0500

    Correct direction for velocity and position.

--- hardware/src/main/kotlin/dev/nextftc/hardware/impl/MotorEx.kt
+++ hardware/src/main/kotlin/dev/nextftc/hardware/impl/MotorEx.kt
@@ -53,7 +53,7 @@ class MotorEx @JvmOverloads constructor(cacheTolerance: Double, motorFactory: ()
      * Gives the unmodified raw tick value of the motor
      */
     val rawTicks: Double
-        get() = motor.currentPosition.toDouble()
+        get() = motor.currentPosition.toDouble() * direction
 
     private val offsetable = Offsetable { rawTicks }
 
@@ -84,7 +84,7 @@ class MotorEx @JvmOverloads constructor(cacheTolerance: Double, motorFactory: ()
      */
     // Cannot use delegation because that would create motor immediately on field initialization instead of it being lazy.
     override val velocity: Double
-        get() = motor.velocity
+        get() = motor.velocity * direction
 
     /**
      * Gets / sets the current power of the motor (automatically implements power caching)
