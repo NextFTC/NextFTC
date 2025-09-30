3bbc754e34e2742f0ff4b438180b27971325a3ea
Zach Harel <zach@zharel.me>
2025-09-30 11:12:36 -0400

    feat: add command creation functions for instant and delayed execution
    Signed-off-by: Zach Harel <zach@zharel.me>

---
+++ core/src/main/kotlin/dev/nextftc/core/commands/Commands.kt
@@ -0,0 +1,39 @@
+@file:JvmName("Commands")
+package dev.nextftc.core.commands
+
+import dev.nextftc.core.commands.delays.Delay
+import dev.nextftc.core.commands.utility.InstantCommand
+import dev.nextftc.core.commands.utility.LambdaCommand
+import kotlin.time.Duration
+
+/**
+ * Creates a command that runs [action] once.
+ */
+fun runOnce(name: String, action: Runnable) = InstantCommand(name, action)
+
+/**
+ * Creates a command that runs [action] once.
+ */
+fun runOnce(action: Runnable) = InstantCommand(action)
+
+/**
+ * Creates a command that runs [action] repeatedly.
+ */
+fun run(name: String, action: Runnable) =
+    LambdaCommand(name).setUpdate(action).setIsDone { false }
+
+/**
+ * Creates a command that runs [action] repeatedly.
+ */
+fun run(action: Runnable) = run("PerpetualCommand", action)
+
+/**
+ * Creates a command that waits [time] seconds.
+ */
+fun wait(time: Duration) = Delay(time)
+
+/**
+ * Creates a command that waits [timeSeconds] seconds.
+ */
+fun wait(timeSeconds: Double) = Delay(timeSeconds)
+
