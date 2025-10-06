e5602be8b7e212b5330227144ce72958d201de61
Zach Harel <zach@zharel.me>
2025-10-06 17:24:36 -0400

    remove testing dependencies
    Signed-off-by: Zach Harel <zach@zharel.me>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,9 +8,6 @@ description = "The base for NextFTC, a user-friendly library for FTC. Includes c
 dependencies {
     api(libs.nextftc.bindings)
     api(libs.nextftc.control)
-
-    testImplementation(kotlin("test"))
-    testImplementation(libs.kotest.kotest.assertions.core)
 }
 
 nextFTCPublishing {
@@ -23,8 +20,4 @@ kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjvm-default=all", "-Xconsistent-data-class-copy-visibility")
     }
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
